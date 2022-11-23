f6457d1df82b1e12be33dfc7d64e8b5bfba11216
Roomey69 <zamedesbonnett@gmail.com>
2022-11-23 09:35:20 +0100

    commit

--- TP7/Exercice2.c
+++ TP7/Exercice2.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(int argc, char const *argv[])
-{
-    
-    printf("Hello");
+int input(char phrase[]){
+    int x;
+    printf("%s",phrase);scanf("%d",&x);
+    return x;
+}
+
+void random(int n, int i){
+    srand((unsigned int)time(NULL));
+    printf("%d", n + (rand() % i));
+}
+
+int main(){
+
+    int n = input("Entrez votre 1er entier : ") ,i = input("Entrez votre 2eme entier : ");
+    random(n,i);
+
     return 0;
 }
